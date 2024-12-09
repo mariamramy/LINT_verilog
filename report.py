e04cff879f23862b4529a8ee928797086d2dda60
janaelshafie <168121874+janaelshafie@users.noreply.github.com>
2024-12-09 22:25:15 +0200

    Delete report.py

--- report.py
+++
@@ -1,5 +0,0 @@
-def report_generator(statement_lists):
-    with open('report.txt', 'w') as f:
-        for item in statement_lists:
-            f.write("%s\n" % item)
-    print("Report Generated Successfully")
